064e48954c5dddcbbb02fdc53f0baef79baaacbf
Leam Hall <leamhall@gmail.com>
2019-02-06 06:20:20 -0500

    Starting the recruiting page.

--- cmd/crewgen/main.go
+++ cmd/crewgen/main.go
@@ -3,18 +3,43 @@ package main
 import (
   "fmt"
 	//"io/ioutil"
+  "html/template"
   "log"
 	"net/http"
 
 	"github.com/makhidkarun/crewgen/pkg/person"
 )
 
-func showCrew(w http.ResponseWriter, r *http.Request) {
+type Page struct {
+  Title string
+  Body []byte
+}
+
+var templateDir string = "../../web/"
+
+func loadPage(title string) (*Page, error) {
 	p := person.Person{Name: "Al Lefron"}
-  fmt.Fprintf(w, "Hey, I'm %s.", p.Name)
+  body := p.Name
+  return &Page{Title: title, Body: []byte(body)}, nil
+}
+ 
+func recruitCrew(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "<h1>Recruiting </h1>"           +
+    "<form action=\"showCrew\" method=\"POST\">"   +
+    "<input type=\"text\" name=\"shipName\"</br>" +
+    "<input type=\"submit\" value=\"Recruit!\">"  +
+    "</form>")
+}
+    
+func showCrew(w http.ResponseWriter, r *http.Request) {
+  title   := "The Crew"
+  page, _ := loadPage(title)
+  t, _    := template.ParseFiles( templateDir + "crew.html")
+  t.Execute(w, page)
 }
 
 func main() {
-  http.HandleFunc("/", showCrew) 
+  http.HandleFunc("/recruit", recruitCrew)
+  http.HandleFunc("/showCrew", showCrew) 
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
