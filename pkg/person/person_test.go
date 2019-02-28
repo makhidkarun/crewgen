a1c00343dff0a74be1776f9494478acfaae678c5
Leam Hall <leamhall@gmail.com>
2019-02-27 20:31:40 -0500

    Moving data/names.db to pkg/person/data. Fixed test to use that.

--- pkg/person/person_test.go
+++ pkg/person/person_test.go
@@ -2,16 +2,17 @@ package person_test
 
 import (
 	"testing"
-
+	"fmt"
 	"github.com/makhidkarun/crewgen/pkg/person"
 )
 
 func TestMakePerson(t *testing.T) {
 	options := make(map[string]string)
 	options["terms"] = "1"
-	options["db_name"] = "../../data/names.db"
+	options["db_name"] = "data/names.db"
 	testP := person.MakePerson(options)
-	if testP.Age == 22 {
+	fmt.Println(testP.Age)
+	if testP.Age <= 21 || testP.Age >= 26 {
 		t.Error(`MakePerson failed age`)
 	}
 }
