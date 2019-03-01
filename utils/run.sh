5d67d2218e6b2170dddee3a0528aa039a98a2f72
Leam Hall <leamhall@gmail.com>
2019-03-01 05:57:21 -0500

    utils/run.sh since I keep forgetting the commands.

---
+++ utils/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run the same build commands
+
+# Starting go vet
+go vet cmd/crewgen/main.go
+
+# Check return
+if ( $? == 0 )
+then
+	# Build
+	time go build -o bin/crewgen cmd/crewgen/main.go 
+	#run
+	bin/crewgen
+fi
