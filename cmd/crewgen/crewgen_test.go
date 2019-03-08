9c27b0a7531aef21ac02206bc5ec0e606ab60a2a
Leam Hall <leamhall@gmail.com>
2019-03-08 05:16:43 -0500

    First effort on httptest of crewgen.

---
+++ cmd/crewgen/crewgen_test.go
@@ -0,0 +1,21 @@
+// crewgen_test.go
+
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandleRecruit(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/recruit", recruitCrew)
+
+	writer = httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/recruit", nil)
+	mux.ServeHTTP(writer, request)
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+}
