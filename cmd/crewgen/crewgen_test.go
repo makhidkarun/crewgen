db64eb850dfa658675f83edb72178b41b910bfaa
Leam Hall <leamhall@gmail.com>
2019-03-08 21:02:34 -0500

    Added a test for show and started using TestMain().

--- cmd/crewgen/crewgen_test.go
+++ cmd/crewgen/crewgen_test.go
@@ -5,17 +5,42 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
+var	mux *http.ServeMux
+var writer *httptest.ResponseRecorder
+
+func TestMain(m *testing.M) {
+	setUp()
+	code := m.Run()
+	os.Exit(code)
+}
+
+func setUp() {
+	templateDir = "../../web"
+}
 
 func TestHandleRecruit(t *testing.T) {
-	templateDir = "../../web"
-	mux := http.NewServeMux()
+	//templateDir = "../../web"
+	mux = http.NewServeMux()
 	mux.HandleFunc("/recruit", recruitCrew)
 
-	writer := httptest.NewRecorder()
+	writer = httptest.NewRecorder()
 	request, _ := http.NewRequest("GET", "/recruit", nil)
+	mux.ServeHTTP(writer, request)
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+}
+
+func TestHandleShow(t *testing.T) {
+	mux = http.NewServeMux()
+	mux.HandleFunc("/show", recruitCrew)
+
+	writer = httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/show", nil)
 	mux.ServeHTTP(writer, request)
 	if writer.Code != 200 {
 		t.Errorf("Response code is %v", writer.Code)
